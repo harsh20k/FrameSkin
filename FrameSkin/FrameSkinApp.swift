95a088181fd0fe26b549613b59cf748ef82839e6
harsh <harsh.pandey77@gmail.com>
2024-05-27 17:38:09 +0530

    Initial Commit

---
+++ FrameSkin/FrameSkinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameSkinApp.swift
+//  FrameSkin
+//
+//  Created by harsh  on 27/05/24.
+//
+
+import SwiftUI
+
+@main
+struct FrameSkinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
